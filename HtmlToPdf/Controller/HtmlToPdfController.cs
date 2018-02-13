febc955c034a224330a10f74d605dc59e9afdb61
david <david@davidmckinnon.ca>
2018-02-12 22:24:10 -0500

    Initial Commit

---
+++ HtmlToPdf/Controller/HtmlToPdfController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf.Controller
+{
+    [EnableCors("CorsPolicy")]
+    public class HtmlToPdfController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/knock")]
+        public string Knock()
+        {
+            return "i got it";
+        }
+
+        [HttpGet]
+        [Route("/api/getnodepdf")]
+        public async Task<byte[]> ExportPdf([FromServices] INodeServices nodeServices)
+        {
+            //http://neil.red/coding/export-html-to-pdf-in-dot-net-core/
+
+            return await nodeServices.InvokeAsync<byte[]>("./pdf", "This is a test PDF o-[.__.]-o");
+        }
+    }
+}
